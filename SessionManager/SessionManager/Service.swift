fa8b7385a0d406f43dddc015b2d642e71112c4b4
Mayank Goyal <mayank.goyal@ibm.com>
2020-06-10 13:58:16 +0400

    - Initial commit of session manager framework

---
+++ SessionManager/SessionManager/Service.swift
@@ -0,0 +1,9 @@
+//
+//  Service.swift
+//  SessionManager
+//
+//  Created by Mayank Goyal on 10/06/2020.
+//  Copyright © 2020 Mayank Goyal. All rights reserved.
+//
+
+import Foundation
---
+++ SessionManager/SessionManager/SessionManager.h
@@ -0,0 +1,19 @@
+//
+//  SessionManager.h
+//  SessionManager
+//
+//  Created by Mayank Goyal on 10/06/2020.
+//  Copyright © 2020 Mayank Goyal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SessionManager.
+FOUNDATION_EXPORT double SessionManagerVersionNumber;
+
+//! Project version string for SessionManager.
+FOUNDATION_EXPORT const unsigned char SessionManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SessionManager/PublicHeader.h>
+
+
